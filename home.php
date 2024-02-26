fb6616f0dee831096c5b7e8bc2fc1916826e7bf4
shrutib2002 <shrutib9393@gmail.com>
2024-02-26 15:54:09 +0530

    added home.php

---
+++ home.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="./styles/Hstyle.css" />
+    <title>Dashboard</title>
+  </head>
+  <body style="background-color: #E9EDC9">
+<div id="sidebar" style="  background: linear-gradient(to bottom,#CCD5AE,#D4A373)
+">
+      <header></header>
+      <ul>
+      <li>
+              <a href="home.php">
+              <i class="fa-solid fa-house"></i>
+                  <span>Home</span>
+              </a>
+          </li>
+          <li>
+              <a href="students.php">
+                  <i class="fa-solid fa-users"></i>
+                  <span>Students</span>
+              </a>
+          </li>
+          <li>
+              <a href="professor.php">
+                  <i class="fa-solid fa-chalkboard-user"></i>
+                  <span>Professor</span>
+              </a>
+          </li>
+          <li>
+              <a href="course.php">
+                  <i class="fa-solid fa-book-open-reader"></i>
+                  <span>Courses</span>
+              </a>
+          </li>
+          <li>
+              <a href="attendance.php">
+                  <i class="fa-solid fa-clipboard-user"></i>
+                  <span>Attendance</span>
+              </a>
+          </li>
+          <li>
+              <a href="academic.php">
+              <i class="fa-solid fa-calendar-days"></i>
+                  <span>Academic Year</span>
+              </a>
+          </li>
+          <li>
+              <a href="marks.php">
+                  <i class="fa-solid fa-paste"></i>
+                  <span>Marks</span>
+              </a>
+          </li>
+          <!-- Add more options as needed -->
+      </ul>
+      <footer></footer>
+  </div>
+  <div id="content">
+      <!-- Main content goes here -->
+      <h1>Main Content</h1>
+      <p>This is the main content area.</p>
+  </div>
+  
+  </body>
+</html>
